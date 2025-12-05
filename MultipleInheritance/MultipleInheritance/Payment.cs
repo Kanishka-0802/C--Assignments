5ff962d9b4971692828ec97f85db3cc1ae8da551
Kanishka-0802 <Kanishka.Chandran@Infinite.com>
2025-12-05 11:02:33 +0530

    Add files via upload

---
+++ MultipleInheritance/MultipleInheritance/Payment.cs
@@ -0,0 +1,124 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MultipleInheritance
+//{
+//   interface IPayment
+//    {
+//        void GetPaymentInfo();
+//     }
+
+//    public class CreditCardPayment : IPayment
+//    {
+//        public string Name { get; set; }
+//        public int PinNumber { get; set; }
+//        public int amount { get; set; }
+//public void GetPaymentInfo()
+//        {
+//            Console.WriteLine("======= Enter the Details for CreditCard ========");
+//            Console.WriteLine("Enter the Name of the Holder : ");
+//            Name = Console.ReadLine();
+//            Console.WriteLine("Enter the PinNumber");
+//            PinNumber = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Enter the Amount : ");
+//            amount = Convert.ToInt32(Console.ReadLine());
+//        }
+//        public void DisplayPayment()
+//        {
+//            Console.WriteLine("===== Smart Payment Gateway System ======");
+//            Console.WriteLine("Name  :" + Name);
+//            Console.WriteLine("PinNumber :"+PinNumber);
+//            Console.WriteLine("Amount : " + amount);
+
+//        }
+//    }
+// public class UPIPayment : IPayment {
+//        public string Name {  get; set; }
+//        public int PinNumber {  get; set; }
+//        public int amount {  get; set; }
+//        public int Accountno {  get; set; }
+//        public  int balance { get; set; }
+
+//       public  void GetPaymentInfo()
+//        {
+//            Console.WriteLine("======== Enter the Details for UPI ========");
+//            Console.WriteLine("Enter the Name of the Holder : ");
+//            Name = Console.ReadLine();
+//            Console.WriteLine("Enter the PinNumber");
+//            PinNumber = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Enter the Amount : ");
+//            amount = Convert.ToInt32(Console.ReadLine());
+//        }
+
+//       public  void GetPaymentPaypal()
+//        {
+//            Console.WriteLine("Enter the Account No : ");
+//            Accountno = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Enter the balance Amount : ");
+//            balance = Convert.ToInt32(Console.ReadLine());
+
+//        }
+//        public void DisplayPayment()
+//        {
+//            Console.WriteLine("Smart Payment Gateway System =======");
+//            Console.WriteLine("Name  :" + Name);
+//            Console.WriteLine("PinNumber :" + PinNumber);
+//            Console.WriteLine("Amount : " + amount);
+//            Console.WriteLine("Account Number : "+Accountno);
+//            Console.WriteLine("Balance : " + balance);
+
+
+//        }
+
+//    }
+//   public  class  WalletPayment : IPayment {
+//        public string Name { get; set; }
+//        public int amount { get; set; }
+//      public   void GetPaymentInfo()
+//        {
+//            Console.WriteLine("Enter the Payment for Wallet :");
+//            Console.WriteLine("Enter the Name of the Holder : ");
+//            Name = Console.ReadLine();
+//            Console.WriteLine("Enter the Amount : ");
+//            amount = Convert.ToInt32(Console.ReadLine());
+//        }
+//        public void DisplayPayment()
+//        {
+//            Console.WriteLine("Smart Payment Gateway System");
+//            Console.WriteLine("Name  :" + Name);
+//            Console.WriteLine("Amount : " + amount);
+
+//        }
+
+//    }
+//   public class DebitCardPayment : IPayment {
+//        public string Name { get; set; }
+//        public int PinNumber { get; set; }
+//        public int amount { get; set; }
+//       public  void GetPaymentInfo()
+//        {
+//            Console.WriteLine("Enter the Payment for DebitCard :");
+//            Console.WriteLine("Enter the Name of the Holder : ");
+//            Name = Console.ReadLine();
+//            Console.WriteLine("Enter the PinNumber");
+//            PinNumber = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Enter the Amount : ");
+//            amount = Convert.ToInt32(Console.ReadLine());
+//        }
+//        public void DisplayPayment()
+//        {
+//            Console.WriteLine("Smart Payment Gateway System");
+//            Console.WriteLine("Name  :" + Name);
+//            Console.WriteLine("PinNumber :" + PinNumber);
+//            Console.WriteLine("Amount : " + amount);
+
+//        }
+//    }
+
+//    }
+
+
+
---
+++ MultipleInheritance/MultipleInheritance/Program.cs
@@ -0,0 +1,141 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MultipleInheritance
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            CreditCardPayment  creditCardPayment = new CreditCardPayment();
+//            UPIPayment upiPayment=new UPIPayment();
+//            DebitCardPayment debitCardPayment = new DebitCardPayment();
+//            WalletPayment walletPayment = new WalletPayment();
+//            creditCardPayment.GetPaymentInfo();
+//            creditCardPayment.DisplayPayment();
+
+//            //debitCardPayment.GetPaymentInfo();
+//            //debitCardPayment.DisplayPayment();
+
+//            //walletPayment.GetPaymentInfo();
+//            //walletPayment.DisplayPayment();
+
+//            //upiPayment.GetPaymentInfo();
+//            //upiPayment.GetPaymentPaypal();
+//            //upiPayment.DisplayPayment();
+
+//            Console.ReadLine();
+
+
+//        }
+//    }
+//}
+
+
+
+using MultipleInheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance
+{
+
+    interface IPayment
+    {
+        void MakePayment(decimal amount);
+    }
+
+    public class CreditCardPayment : IPayment
+    {
+      
+       
+        public void MakePayment(decimal amount)
+        {
+            Console.WriteLine($"Processing  CreditCardpayment of {amount :C}.");
+        }
+
+     
+    }
+    public class PaypalPayment : IPayment,ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"[LOG]{DateTime.Now}:{message}");
+
+        }
+
+        public void MakePayment(decimal amount)
+        {
+            Console.WriteLine($"Processing  Paypalpayment of {amount:C}.");
+        }
+        public void Refundpayment(decimal amount)
+        {
+            Console.WriteLine($"Refunding payment of {amount:C} through Paypal");
+        }
+     
+    }
+    public class WalletPayment : IPayment
+    {
+
+
+        public void MakePayment(decimal amount)
+        {
+            Console.WriteLine($"Processing  Walletpayment of {amount:C}.");
+        }
+
+    }
+
+
+
+
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+      
+        Console.WriteLine("Enter the Payment Option would you choose : ");
+        int choice = Convert.ToInt16(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                PaypalPayment paypal = new PaypalPayment();
+                paypal.MakePayment(100);
+                paypal.Refundpayment(500.45m);
+                paypal.Log("Payment of 1000 made through Paypal.");
+                break;
+
+            case 2:
+                IPayment creditcard = new CreditCardPayment();
+                creditcard.MakePayment(100);
+                break;
+            case 3:
+                IPayment wallet = new WalletPayment();
+                wallet.MakePayment(3000);
+                break;
+            default:
+                Console.WriteLine("Invalid Choice");
+                break;
+
+        }
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+
+
+
+
